5926385e09a035c7e72853096b5a64b1973d2a60
Suslov <suslov@metasharks.ru>
2022-01-29 01:06:43 +0200

    Коммит шаблона

---
+++ adminLte/database/migrations/2022_01_28_001207_create_customers_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->string('surname', 255);
+            $table->string('email', 255);
+            $table->string('phone_number', 255);
+        });
+
+        Schema::create('company_customer', static function (Blueprint $table): void {
+            $table
+                ->foreignId('customer_id')
+                ->references('id')
+                ->on('customers')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table
+                ->foreignId('company_id')
+                ->references(
+                    'id'
+                )->on('companies')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+
+            $table->primary(['customer_id', 'company_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+        Schema::dropIfExists('company_client');
+    }
+}
