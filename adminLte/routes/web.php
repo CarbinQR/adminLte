c77e66dcf0afd094f489fe769e3a3f179ba0e2c9
Suslov <suslov@metasharks.ru>
2022-01-29 02:15:37 +0200

    Фикс

--- adminLte/routes/web.php
+++ adminLte/routes/web.php
@@ -16,12 +16,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('pages.home');
+})->name('home');
 
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::prefix('companies')->group(function () {
     Route::get('/', [CompanyController::class, 'index'])->name('companiesList');
